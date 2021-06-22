d031668c754d01f3f239d1200b97cf03c13ab996
Achraf RACHIDI <achraf.rachidi@easii-ic.com>
2021-06-22 16:35:44 +0200

    update

--- Mes tests/test.py
+++ Mes tests/test.py
@@ -3,49 +3,74 @@
 from tkinter import *
 import json
 from decimal import Decimal
+from PIL import Image, ImageTk
 #Variable à retourner
 result = {}
 
 class Table:
       
-    def __init__(self,root, data, rows, columns):
-        root.geometry("800x800")
-        
+    def __init__(self,root, data):
+        self.root = root
+        self.root.geometry("800x800")
+        self.header = []
+        self.gridRow = 0
+        self.gridColumn = 0
+        #create Space
+        #self.createSpace(10)
         # code for creating table
-        self.label1 = Label(root, text="Name")
-        self.label1.grid(row=0, column=0)
-        self.label2 = Label(root, text="Location")
-        self.label2.grid(row=0, column=1)
-        self.label3 = Label(root, text="Value(R)")
-        self.label3.grid(row=0, column=2)
-        self.label4 = Label(root, text="Value(V)")
-        self.label4.grid(row=0, column=3)
-        self.label5 = Label(root, text="Result")
-        self.label5.grid(row=0, column=4)
-        i = 10
-        for dt in data:
-            self.Name = Label(root, text=dt['name'] )
-            self.Name.grid(row=i, column=0)
-            self.loc = Label(root, text=dt['location'], bg=dt['color'])
-            self.loc.grid(row=i, column=1)
-            varOhm = StringVar()
-            self.valOhm = Entry(root, textvariable = varOhm)
-            self.valOhm.grid(row=i, column=2)
-            varOhm.trace("w", lambda name, index, mode, \
-            varOhm=varOhm, max=dt['ValueOhm']*1.2, min=dt['ValueOhm']*0.8, entry=self.valOhm, nm=dt['name'], type="Ohm": \
-                self.eventHandlerEntry(varOhm, max, min, entry, nm , type))
+        self.createImage("easii-ic.png")
+        self.createSpace(10)
+        self.writeHeader(data)
+        self.writeContent(data)
+    
+    def createSpace(self, nbrSpace):
+        for i in range(nbrSpace) :
+            lb = Label(self.root, text=" ")
+            lb.grid(row=i, column=0)
+            self.gridRow += 1
 
+    def createImage(self, path=""):
+        self.img2 = ImageTk.PhotoImage(file = path)
+        self.labelEntr = Label(self.root, image=self.img2)
+        self.labelEntr.place(x=0, y=0, relwidth=1, relheight=0.3)
+
+    #def createButton(self):
+
+
+    def writeContent(self, data):
+        for li in data:
+            keys = li.keys()
+            self.gridColumn = 0
+            for key in keys:
+                if(isinstance(li[key], list)):
+                    self.writeInputs(li[key], li['name'])
+                else:
+                    self.writeLabels(key, li)
+            self.gridRow += 1
+
+    def writeInputs(self, data, name):
+        result[name] = {}
+        for li in data:
             varV = StringVar()
-            self.valV = Entry(root, textvariable = varV)
-            self.valV.grid(row=i, column=3)
-            self.result = Label(root, text="")
-            self.result.grid(row=i, column=4)
+            self.valV = Entry(self.root, textvariable = varV, borderwidth=1, relief="ridge", width=25)
+            self.valV.grid(row=self.gridRow, column=self.gridColumn)
+            self.gridColumn += 1
+
             varV.trace("w", lambda name, index, mode, \
-            varV=varV, max=dt['ValueT']*1.05, min=dt['ValueT']*0.95, entry=self.valV, nm=dt['name'], type="V": \
+            varV=varV, max=li['max'], min=li['min'], entry=self.valV, nm=name, type=li['unit']: \
                 self.eventHandlerEntry(varV, max, min, entry, nm , type))
-            types = {"Ohm" : "", "V": ""}
-            result[dt['name']] = types
-            i += 1
+            result[name][li['unit']] = ""
+
+
+
+    def writeLabels(self, key, data):
+        if key != "color":
+            if key == "location":
+                lb = Label(self.root, text=data[key], bg=data["color"], borderwidth=1, relief="ridge", width=20)
+            else:
+                lb = Label(self.root, text=data[key], borderwidth=1, relief="ridge", width=20)
+            lb.grid(row=self.gridRow, column=self.gridColumn)
+            self.gridColumn += 1
 
 
     def eventHandlerEntry(self, element, max, min, entry, name, type):
@@ -57,15 +82,31 @@ class Table:
             else:
                 entry.configure({"background": "green"})
         result[name][type]= element.get()  
+
+    def writeHeader(self, data, colum=0):
+        li = data[0].keys()
+        for element in li:
+            if isinstance(data[0][element], list):
+                for dt in data[0][element]:
+                    self.header.append(Label(self.root, text=dt["unit"], borderwidth=1, relief="ridge", width=20))
+                    self.header[colum].grid(row=self.gridRow, column=colum)
+                    colum += 1
+            elif element != "color":
+                self.header.append(Label(self.root, text=element, borderwidth=1, relief="ridge", width=20))
+                self.header[colum].grid(row=self.gridRow, column=colum)
+                colum += 1
+        self.header.append(Label(self.root, text="Résult", borderwidth=1, relief="ridge", width=20))
+        self.header[colum].grid(row=self.gridRow, column=colum)
+        self.gridRow += 1
+
+
   
 def test():
-    f = open('data.json',)
+    f = open('data copy.json',)
     data = json.load(f)
     f.close()
-    total_rows = len(data['composant'])
-    total_columns = 5
     root = Tk()
-    my_gui = Table(root, data['composant'], total_rows, total_columns)
+    my_gui = Table(root, data['composant'])
     root.mainloop()
 test()
    
